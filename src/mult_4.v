4d6d872b9581808258edd4d7ac26c6fde3809f42
JohanRuiz05 <68175218+JohanRuiz05@users.noreply.github.com>
2025-12-01 08:25:28 -0500

    Update mult_4.v

--- src/mult_4.v
+++ src/mult_4.v
@@ -1,4 +1,42 @@
-module mult_4(clk , rst , init , A , B , pp , done);
+`default_nettype none
+
+module tt_um_example (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  reg resetn;
+  /* verilator lint_on SYNCASYNCNET */
+  always @(negedge clk) resetn <= rst_n;
+
+  // All output pins must be assigned. If not used, assign to 0
+  assign uio_in[7:1] = 0;
+  assign uio_out[7:2] = 0;
+  assign uio_out[0] = 0;
+  assign uio_oe  = 0;
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, uio_oe[7:0],uio_in[7:1], uio_out[7:2], uio_out[0] 1'b0};
+
+  mult_4 mult_4(
+      .clk(clk),
+      .rst(resetn),
+      .init(uio_in[0]),
+      .A(ui_in[3:0]),
+      .B(ui_in[7:4]),
+      .pp(uo_out[7:0]),
+      .done(uio_out[1])
+   );
+
+endmodule
+  
+  module mult_4 (clk , rst , init , A , B , pp , done);
   
   input clk;
   input rst;
